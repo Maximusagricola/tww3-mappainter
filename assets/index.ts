ec507f3a0c93817e72c2ccaf8d1bd88d5b94886d
Maximusagricola <eugeneharris@outlook.com>
2025-06-24 21:42:52 +0100

    Delete assets directory

--- assets/index.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-const requireAsset = require.context('.', true, /^.+\.png$/);
-const assets = requireAsset.keys().reduce((accumulator, imageName) => {
-  const [/* fullMatch */, key] = imageName.match(/^\.\/(.+)\.png$/);
-  accumulator[key] = requireAsset(imageName);
-  return accumulator;
-}, {});
-
-export default assets as Record<string, string>;
