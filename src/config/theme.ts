f2298db4a0f446c61e527673569ccc1f9575d384
Maximusagricola <eugeneharris@outlook.com>
2025-07-06 13:37:44 +0100

    Add files via upload

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,9 +1,9 @@
-import { createMuiTheme } from '@material-ui/core';
-import { blueGrey } from '@material-ui/core/colors';
+import { createTheme } from '@mui/material/styles';
+import { blueGrey } from '@mui/material/colors';
 
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
-    type: 'dark',
+    mode: 'dark',
     primary: {
       main: blueGrey[300],
     },
