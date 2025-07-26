3c14e803298dea6ad442405409cd8a1a2817da74
Maximusagricola <eugeneharris@outlook.com>
2025-07-26 14:58:30 +0100

    Update BarContent.tsx

--- src/components/BarContent.tsx
+++ src/components/BarContent.tsx
@@ -2,19 +2,11 @@ import { makeStyles } from '@mui/styles';
 import { IconButton, Typography, Toolbar } from '@mui/material';
 import type { Theme } from '@mui/material/styles';
 import TuneIcon from '@mui/icons-material/Tune';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import HelpIcon from '@mui/icons-material/Help';
 
 const useStyles = makeStyles((theme: Theme) => ({
-
   title: {
     display: 'flex',
     flexDirection: 'column',
-  },
-  github: {
-    [theme.breakpoints.up('md')]: {
-      marginRight: -12,
-    },
   },
   fillSpace: {
     flexGrow: 1,
@@ -45,25 +37,6 @@ function BarContent(props: BarContentProps) {
         </Typography>
       </div>
       <span className={classes.fillSpace}></span>
-      <IconButton
-        color="inherit"
-        component="a"
-        href="https://steamcommunity.com/sharedfiles/filedetails/?id=2244941200"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <HelpIcon />
-      </IconButton>
-      <IconButton
-        className={classes.github}
-        color="inherit"
-        component="a"
-        href="https://github.com/IMRZ/tww2-map-painter"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <GitHubIcon />
-      </IconButton>
       <IconButton
         className={classes.toggleButton}
         color="inherit"
